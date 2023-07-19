41ce60d65c57e17ab8b4c6c23fdafa85546f936c
aytanumudova <aytanumudova@gmail.com>
2023-07-19 22:21:00 +0400

    introsection was done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./components/navbar";
+import Introsection from "./components/introsection";
 function App() {
   return (
-    <div className="mainParent">
+    <>
       <Navbar></Navbar>
-      <div className="container"></div>
-    </div>
+      <Introsection></Introsection>
+    </>
   );
 }
 
