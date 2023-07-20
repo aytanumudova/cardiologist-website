bddec466b072c719b2c77de0e4f6831e015f9d45
aytanumudova <aytanumudova@gmail.com>
2023-07-20 20:02:28 +0400

    whatPatientsSay Section was completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./components/navbar";
 import Introsection from "./components/introsection";
+import WhatPatientsSay from "./components/whatPatientsSay";
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Introsection></Introsection>
+      <WhatPatientsSay></WhatPatientsSay>
     </>
   );
 }
